b084d74100424611f08f385c92908b98b581147d
jeko7411 <jeancarlos7411@gmail.com>
2021-03-23 15:34:06 -0500

    version 0001

---
+++ templates/bienvenida.html.tpl
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <title>{$t}</title>
+    {literal}
+    <style>
+        body {
+            background-color: #3b3c36;
+        }
+    </style>
+    {/literal}
+</head>
+
+<body>
+
+   
+       
+            
+
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ templates_c/%%36^36F^36F27CE3%%bienvenida.html.tpl.php
@@ -0,0 +1,31 @@
+<?php /* Smarty version 2.6.20, created on 2021-03-23 07:05:08
+         compiled from bienvenida.html.tpl */ ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $this->_tpl_vars['t']; ?>
+</title>
+    <?php echo '
+    <style>
+        body {
+            background-color: #3b3c36;
+        }
+    </style>
+    '; ?>
+
+</head>
+
+<body>
+
+   
+       
+            
+
+        </div>
+    </div>
+
+</body>
+
+</html>
